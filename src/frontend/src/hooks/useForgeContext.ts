1857188a5c74f7cbed8edb44774a3bbdb132ac21
Mark <mark@remarkablemark.org>
2024-03-10 14:51:21 -0400

    refactor(frontend): tidy logs and generate .env.local

--- src/frontend/src/hooks/useForgeContext.ts
+++ src/frontend/src/hooks/useForgeContext.ts
@@ -1,17 +1,14 @@
 import { view } from '@forge/bridge';
 import { useEffect, useState } from 'react';
 
+import { log } from '../helpers';
 import type { FullContext } from '../types';
 
 export function useForgeContext() {
   const [context, setContext] = useState<FullContext | undefined>();
 
   useEffect(() => {
-    view
-      .getContext()
-      .then(setContext)
-      // eslint-disable-next-line no-console
-      .catch((error) => console.error('[ISSUE FORMULA]', error));
+    view.getContext().then(setContext).catch(log.error);
   }, []);
 
   return context;
--- src/frontend/src/hooks/useJiraSearch.ts
+++ src/frontend/src/hooks/useJiraSearch.ts
@@ -1,6 +1,7 @@
 import { requestJira } from '@forge/bridge';
 import { useEffect, useState } from 'react';
 
+import { log } from '../helpers';
 import type { Payload } from '../types';
 
 export function useJiraSearch(payload: Payload) {
@@ -20,8 +21,7 @@ export function useJiraSearch(payload: Payload) {
     })
       .then((response) => response.json())
       .then(setData)
-      // eslint-disable-next-line no-console
-      .catch((error) => console.error('[ISSUE FORMULA]', error));
+      .catch(log.error);
   }, [data, payload]);
 
   return data;
