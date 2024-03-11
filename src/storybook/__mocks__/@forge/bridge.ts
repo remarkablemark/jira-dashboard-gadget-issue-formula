ba083f3df99422e0e149358564579662d485650d
Mark <mark@remarkablemark.org>
2024-03-11 17:40:55 -0400

    chore(storybook): improve @forge/bridge mock

--- src/storybook/__mocks__/@forge/bridge.ts
+++ src/storybook/__mocks__/@forge/bridge.ts
@@ -3,12 +3,12 @@ import { fn } from '@storybook/test';
 export const requestJira = fn();
 
 export const view = {
-  submit: fn(),
-  close: fn(),
-  refresh: fn(),
-  createHistory: fn(),
-  getContext: fn(),
+  submit: fn(() => Promise.resolve()),
+  close: fn(() => Promise.resolve()),
+  refresh: fn(() => Promise.resolve()),
+  createHistory: fn(() => Promise.resolve()),
+  getContext: fn(() => Promise.resolve()),
   theme: {
-    enable: fn(),
+    enable: fn(() => Promise.resolve()),
   },
 };
