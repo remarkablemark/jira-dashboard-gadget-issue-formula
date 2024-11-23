f7905d2a93f2559b402c9a223c5b62cc79c4c132
Mark <mark@remarkablemark.org>
2024-11-23 00:52:17 -0500

    test(frontend): set up and add test

---
+++ src/frontend/test/setupTests.ts
@@ -0,0 +1,15 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+
+jest.mock('@forge/bridge', () => ({
+  requestJira: jest.fn().mockResolvedValue({ json: jest.fn() }),
+  view: {
+    getContext: jest.fn().mockResolvedValue(undefined),
+    submit: jest.fn(),
+  },
+}));
+
+jest.mock('../src/env');
