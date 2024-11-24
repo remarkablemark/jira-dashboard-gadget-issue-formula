c08738d1b38c694b10f3b87b46f9a784fca170b1
Mark <mark@remarkablemark.org>
2024-11-24 12:59:50 -0500

    test(frontend): update Edit.test.tsx

--- src/frontend/src/edit/Edit.test.tsx
+++ src/frontend/src/edit/Edit.test.tsx
@@ -8,10 +8,18 @@ const mockedView = jest.mocked(view);
 
 // with no data
 describe('new gadget', () => {
+  let consoleErrorSpy: jest.SpyInstance;
+
   beforeEach(() => {
     mockedView.getContext.mockResolvedValueOnce({
       extension: { gadgetConfiguration: undefined },
     } as unknown as FullContext);
+    // suppress React deprecated Context API error
+    consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
+  });
+
+  afterEach(() => {
+    consoleErrorSpy.mockRestore();
   });
 
   it('renders headings', async () => {
@@ -34,16 +42,31 @@ describe('new gadget', () => {
     });
   });
 
-  it('adds variable', async () => {
+  it('adds variable row', async () => {
     render(<Edit />);
-    // suppress React deprecated Context API error
-    const spy = jest.spyOn(console, 'error').mockImplementation();
     fireEvent.click(screen.getByRole('button', { name: 'Add variable' }));
-    spy.mockRestore();
     await waitFor(() => {
-      ['Variable', 'Function', 'JQL'].forEach((label) => {
-        expect(screen.getByLabelText(label)).toBeInTheDocument();
-      });
+      expect(screen.getAllByRole('textbox')).toHaveLength(2);
     });
+    ['Variable', 'Function', 'JQL'].forEach((label) => {
+      expect(screen.getByLabelText(label)).toBeInTheDocument();
+    });
+    // Function
+    expect(screen.getByDisplayValue('COUNT')).toBeInTheDocument();
+  });
+
+  it('adds formula row', async () => {
+    render(<Edit />);
+    fireEvent.click(screen.getByRole('button', { name: 'Add formula' }));
+    await waitFor(() => {
+      expect(screen.getAllByRole('textbox')).toHaveLength(6);
+    });
+    ['Math Formula', 'Label', 'Decimals', 'Prefix', 'Suffix'].forEach(
+      (label) => {
+        expect(screen.getByLabelText(label)).toBeInTheDocument();
+      },
+    );
+    // Decimals
+    expect(screen.getByDisplayValue('0')).toBeInTheDocument();
   });
 });
