4b69cc9399fd1b8765f007ac2f874483208c65b7
Mark <mark@remarkablemark.org>
2024-11-24 03:41:18 -0500

    test(frontend): update Edit.test.tsx

--- src/frontend/src/edit/Edit.test.tsx
+++ src/frontend/src/edit/Edit.test.tsx
@@ -1,33 +1,49 @@
 import { view } from '@forge/bridge';
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 
 import type { FullContext } from '../types';
 import Edit from './Edit';
 
 const mockedView = jest.mocked(view);
 
-it('renders headings', async () => {
-  mockedView.getContext.mockResolvedValueOnce({
-    extension: { gadgetConfiguration: undefined },
-  } as unknown as FullContext);
-  render(<Edit />);
-  await waitFor(() => {
-    ['Variables', 'Formulas'].forEach((name) => {
-      expect(
-        screen.getByRole('heading', { level: 3, name }),
-      ).toBeInTheDocument();
+// with no data
+describe('new gadget', () => {
+  beforeEach(() => {
+    mockedView.getContext.mockResolvedValueOnce({
+      extension: { gadgetConfiguration: undefined },
+    } as unknown as FullContext);
+  });
+
+  it('renders headings', async () => {
+    render(<Edit />);
+    await waitFor(() => {
+      ['Variables', 'Formulas'].forEach((name) => {
+        expect(
+          screen.getByRole('heading', { level: 3, name }),
+        ).toBeInTheDocument();
+      });
     });
   });
-});
 
-it('renders buttons', async () => {
-  mockedView.getContext.mockResolvedValueOnce({
-    extension: { gadgetConfiguration: undefined },
-  } as unknown as FullContext);
-  render(<Edit />);
-  await waitFor(() => {
-    ['Save', 'Cancel'].forEach((name) => {
-      expect(screen.getByRole('button', { name })).toBeInTheDocument();
+  it('renders buttons', async () => {
+    render(<Edit />);
+    await waitFor(() => {
+      ['Save', 'Cancel'].forEach((name) => {
+        expect(screen.getByRole('button', { name })).toBeInTheDocument();
+      });
+    });
+  });
+
+  it('adds variable', async () => {
+    render(<Edit />);
+    // suppress React deprecated Context API error
+    const spy = jest.spyOn(console, 'error').mockImplementation();
+    fireEvent.click(screen.getByRole('button', { name: 'Add variable' }));
+    spy.mockRestore();
+    await waitFor(() => {
+      ['Variable', 'Function', 'JQL'].forEach((label) => {
+        expect(screen.getByLabelText(label)).toBeInTheDocument();
+      });
     });
   });
 });
